0dc3b09f572524f571ed841f178226e5a55d28de
Matheus Gois <mattheusgoiis@gmail.com>
2023-08-21 18:02:05 -0300

    feat/ unlike

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,15 +4,19 @@ class FavoritesController < ApplicationController
     def like
         @enterprise = Enterprise.find(params[:enterprise_id])
         @user = User.find(params[:user_id])
-        @favorite = Favorite.new()
-        @favorite.user = @user
-        @favorite.enterprise = @enterprise
+        @favorite = Favorite.find_by(user: @user, enterprise: @enterprise)
 
-        if @favorite.save
-            render json: {favorite: @favorite}, status: :ok
+        if @favorite
+            @favorite.destroy
+            render json: {message: "Unfavorited"}, status: :ok
         else
-            render json: {error: "Usuário ou Empreendimento não encontrados!"}, status: :not_found
+            @favorite = Favorite.new(user: @user, enterprise: @enterprise)
+            
+            if @favorite.save
+                render json: {favorite: @favorite}, status: :ok
+            else
+                render json: {error: "Usuário ou Empreendimento não encontrados!"}, status: :not_found
+            end
         end
     end
-
 end
