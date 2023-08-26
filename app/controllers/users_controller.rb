8836736f4dfa87791532503eb7ba6196c6e5e988
Matheus Gois <mattheusgoiis@gmail.com>
2023-08-26 12:37:23 -0300

    feat: add some routes to get info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :authorize, only: [:view_user]
+
     def create
         @user = User.new(user_params)
         if @user.save
@@ -15,7 +17,21 @@ class UsersController < ApplicationController
         @user = User.find_by(email: user_params[:email])
         if @user && @user.authenticate(user_params[:password])
             token = encode_token({user_id: @user.id})
-            render json: {user: @user, token: token},
+            enterprises = Enterprise.where(user_id: @user.id)
+            liked_enterprises = Favorite.where(user_id: @user.id)
+            liked = []
+            user_enterprises = []
+            enterprises.each do |enterprise|
+                user_enterprises.push(enterprise.id)
+            end
+            liked_enterprises.each do |enterprise|
+                liked.push(enterprise.enterprise_id)
+            end
+
+            render json: {user: @user,
+                          token: token,
+                          enterprises: user_enterprises,
+                          liked_enterprises: liked},
             status: :ok
         else
             render json: {error: "Usuário ou senha invalidos"},
@@ -23,9 +39,34 @@ class UsersController < ApplicationController
         end
     end
 
+    def view_user
+        @user = User.find(params[:user_id])
+        enterprises = Enterprise.where(user_id: @user.id)
+        liked_enterprises = Favorite.where(user_id: @user.id)
+        liked = []
+        user_enterprises = []
+        enterprises.each do |enterprise|
+            user_enterprises.push(enterprise.id)
+        end
+        liked_enterprises.each do |enterprise|
+            liked.push(enterprise.enterprise_id)
+        end
+        if @user
+            render json: {
+                user: @user,
+                enterprises: user_enterprises,
+                liked_enterprises: liked,
+            },
+            status: :ok
+        else
+            render json: {error: "Usuário não encontrado!"},
+            status: :not_found
+        end
+    end
+
     private
 
     def user_params
-        params.require(:user).permit(:email,:password)
+        params.require(:user).permit(:email,:password, :name)
     end
 end
