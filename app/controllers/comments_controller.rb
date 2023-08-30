2ac8767aac0f77c111c43d26890ae7b0ae2b60db
Matheus Gois <mattheusgoiis@gmail.com>
2023-08-30 00:23:33 -0300

    feat: add image on user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-
     before_action :authorize
 
     def get_enterprise_comments
@@ -11,7 +10,8 @@ class CommentsController < ApplicationController
                 text_content: comment.text_content,
                 enterprise_id: comment.enterprise_id,
                 user_id: comment.user_id,
-                user_name: comment.user.name
+                user_name: comment.user.name,
+                user_avatar: comment.user.avatar.url
             }
             enterprise_comments << comment_data
         end
